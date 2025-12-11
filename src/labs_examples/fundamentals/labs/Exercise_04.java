0d8539f5dcb01fd67abe4d0f18139d428c0ca9ae
Phillip Patterson <phillip.patterson92@gmail.com>
2025-12-10 20:09:26 -0600

    fundamentals labs ex 4

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,23 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean isTrue = false;
+        System.out.println("isTrue = " + isTrue);
+        char letter = 'a';
+        System.out.println("letter = " + letter);
+        byte byteVal = 111;
+        System.out.println("byteVal = " + byteVal);
+        short shortVal = 3456;
+        System.out.println("shortVal = " + shortVal);
+        int num = 2;
+        System.out.println("num = " + num);
+        long longVal = 4789847879l;
+        System.out.println("longVal = " + longVal);
+        float floatVal = 222.2233333333333f;
+        System.out.println("floatVal = " + floatVal);
+        double doubleVal = 555.55;
+        System.out.println("doubleVal = " + doubleVal);
+
 
     }
 
