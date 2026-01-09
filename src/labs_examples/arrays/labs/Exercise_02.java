0a2a187e93019242a6707e57b54834506a8a2640
Phillip Patterson <phillip.patterson92@gmail.com>
2026-01-09 10:31:05 -0600

    arrays

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  *  More labs_examples.arrays
  *
@@ -15,7 +17,15 @@ public class Exercise_02 {
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         // write code here
+        Scanner scanner = new Scanner(System.in);
 
+        //prompt user
+        System.out.println("Enter a number between 1-10: ");
 
+        //assign input in variable int
+        int input = scanner.nextInt();
+        //TODO: I used stack overflow as a guide
+        int index = array[input];
+        System.out.println("Index: " + index);
     }
 }
--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -3,7 +3,7 @@ package labs_examples.arrays.labs;
 /**
  *  2D Array
  *
- *      Creat and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
+ *      Create and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
  *
  *      The output should look something like this:
  *
@@ -18,6 +18,14 @@ package labs_examples.arrays.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-        
+        int[][] twoD = new int[5][5];
+
+        for (int i = 0; i < twoD.length; i++){
+            for (int j = 0; j < twoD[i].length; j++){
+                twoD[i][j] = 3 * ((i * twoD[i].length) + j + 1);;
+                System.out.print(twoD[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
