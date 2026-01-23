2dd6c0fcc006b94beaf4f96c44f20a8f682c57a6
Phillip Patterson <phillip.patterson92@gmail.com>
2026-01-23 15:42:22 -0600

    methods v1

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
@@ -10,6 +10,19 @@ public class Exercise_01 {
         // please create the methods as instructed below then
         // call each of those methods from here, within the main()
 
+        int prod = multiply(5,4);
+        System.out.println(prod);
+        double result = divide(10, 2);
+        System.out.println(result);
+        printJoke();
+        System.out.println(convertToSeconds(5));
+
+        sumAll(10);
+        sumAll(10,20,30);
+        sumAll(100,150,275, 300, 550);
+
+
+
     }
 
 
@@ -29,8 +42,32 @@ public class Exercise_01 {
 
 
     // 5) Create a varargs method that will return the length of the varargs array passed in
+    public static void sumAll(int a, int... nums){
+        int sum = 0;
+        for (int i : nums){
+            sum += i;
+        }
+        System.out.println("sum equal " + sum);
+    }
 
+    public static int multiply(int a, int b){
+        int val = a * b;
+        System.out.println(val);
+        return val;
+    }
 
+    public static int divide(int a, int b){
+        return a / b;
+    }
+
+    public static void printJoke(){
+        System.out.println("Knock Knock who's there");
+    }
+
+    public static int convertToSeconds(int years){
+        int seconds = 31556952;
+        return years * seconds;
+    }
 
 
 
