b5a79d075b4052f03cb3459900ddbfc924a711ec
Phillip Patterson <phillip.patterson92@gmail.com>
2026-04-24 16:43:31 -0500

    input_output labs

--- src/labs_examples/input_output/labs/Exercise_04.java
+++ src/labs_examples/input_output/labs/Exercise_04.java
@@ -1,5 +1,10 @@
 package labs_examples.input_output.labs;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  *      Following the video examples and source code found in the src/labs_examples/input_output/examples/csv_parser
@@ -12,3 +17,43 @@ package labs_examples.input_output.labs;
  *      Then, write the arraylist of objects back out to a new .csv file. Ensure that the resulting csv file is valid.
  *
  */
+class CSVParser{
+    public static void main(String[] args) {
+        //read file
+        BufferedReader reader = null;
+        String line = "";
+        try {
+            reader = new BufferedReader(new FileReader("/Users/phillippatterson/Documents/CodingNomads/labs/online-java-fundamentals/src/labs_examples/input_output/files/NBA_Player_Stats.csv"));
+            ArrayList<Object> arrayList = new ArrayList<Object>();
+            while ((line = reader.readLine()) != null) {
+                String[] stats = line.split(",");
+                System.out.println(line);
+                NBAPlayerStats nbaPlayer = new NBAPlayerStats();
+                nbaPlayer.name = stats[0];
+                nbaPlayer.points = Double.parseDouble(stats[1]);
+                nbaPlayer.rebounds = Integer.parseInt(stats[2]);
+                nbaPlayer.assists = Integer.parseInt(stats[3]);
+
+                arrayList.add(nbaPlayer);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (Exception e) {
+                System.out.println("Error closing csv");
+            }
+        }
+    }
+}
+
+class NBAPlayerStats{
+    //create variables that match csv fields
+    String name;
+    double points;
+    int rebounds;
+    int assists;
+
+}
